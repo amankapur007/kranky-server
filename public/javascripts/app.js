784ec3342b6a9ddf96ee34c0629bb08adf0a5692
Aman Kapur <akapur07@gmail.com>
2020-12-15 15:26:06 +0530

    SSO

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -167,7 +167,7 @@
 
 	app.controller('LocalStreamController',['camera', '$scope', '$window', function(camera, $scope, $window){
 		var localStream = this;
-		localStream.name = 'Guest';
+		localStream.name = '';
 		localStream.link = '';
 		localStream.cameraIsOn = false;
 
